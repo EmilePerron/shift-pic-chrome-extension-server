5691c217dcdb8fc57dd989593834def17461f514
Émile Perron <contact@emileperron.com>
2020-09-20 14:38:03 +0000

    Fixed an issue where the Resize lib would resize in a 'contain' manner instead of a 'cover' one.

--- app/Lib/Resize.php
+++ app/Lib/Resize.php
@@ -8,7 +8,16 @@ abstract class Resize {
 
     public static function process(string $path, array $settings = [], bool $overwrite = false)
     {
-        $image = (new ImageResize($path))->resizeToBestFit($settings['width'], $settings['height']);
+        $image = new ImageResize($path);
+
+        # Gumlet\ImageResize's resizeToBestFit is for a "contain" mode; we want "cover". Let's do the math ourselves...
+        if (!($image->getSourceWidth() <= $settings['width'] && $image->getSourceHeight() <= $settings['height'])) {
+            $ratio  = $image->getSourceHeight() / $image->getSourceWidth();
+            $width = $settings['width'];
+            $height = $width * $ratio;
+            $image->resize($width, $height, false);
+        }
+
         $image->quality_jpg = 100;
         $image->quality_webp = 100;
         $image->quality_png = 0;
